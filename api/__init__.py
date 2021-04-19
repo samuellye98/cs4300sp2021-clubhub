063b17e207e6e9f811fec8daa551d5721c757a5e
Samuel Lye <sl2982@cornell.edu>
2021-04-19 13:49:15 +0800

    Remove api files

--- api/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-# Define a Blueprint for this module (api)
-api = Blueprint('api', __name__, url_prefix='/api')
-# Import + Register Blueprints
-
-# from api.accounts import accounts as accounts
-# from api.irsystem import irsystem as irsystem
